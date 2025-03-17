0710fb677a6143f651e14928572d04f2ac130b2b
Hasnain Sharif <186687737+hasnainsharif1@users.noreply.github.com>
2025-03-17 05:43:29 +0500

    half mini project update

--- 02_Student_manag_system/Std_mang_sys.py
+++ 02_Student_manag_system/Std_mang_sys.py
@@ -1,8 +1,13 @@
+import os
+
 Student_data_list = []
 
-
+def static_interface():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 while True:
+
+    static_interface()
     print("-------- Student Managment System -------- ")
     print("1.Add Student")
     print("2.Update Student")
@@ -14,21 +19,50 @@ while True:
     user_input = int(input("Enter Your Choice: "))
 
     if user_input == 1:
+        static_interface()
         std_dict = {}
-        name = input('Enter Student Name: ')
-        roll_no = input("Enter Roll Number: ")
-        age = input("Enter Age: ")
-        grades = input("Enter Grades: ")
+        roll_no = int(input("Enter Roll Number: "))
+        name = str(input('Enter Student Name: '))
+        age = int(input("Enter Age: "))
+        grades = int(input("Enter Grades: "))
 
-        std_dict['name', 'roll_no', 'age', 'grades'] = name, roll_no, age, grades
+        std_dict['name'] = name
+        std_dict['roll_no'] = roll_no
+        std_dict['age'] = age
+        std_dict['grades'] = grades
         Student_data_list.append(std_dict)
+        print("Student Data Added Successfully!")
         print(Student_data_list)
-        break
+    elif user_input == 2:
+        std_id = int(input("Enter Student Id Or Name: "))
+        found = False
 
+        for student in Student_data_list:
+            if student.get('roll_no') == std_id:
+                name = str(input('Enter Student Name: '))
+                age = int(input("Enter Age: "))
+                grades = int(input("Enter Grades: "))
 
+                student['name'] = name
+                student['age'] = age
+                student['grades'] = grades
 
+                print("Student Data updated successully!")
+                found = True
+                break  
+        if found == False:
+            print("Student not Found!")
+    elif user_input == 3:
+        std_id = int(input("Enter Student Id Or Name: "))
+        found = False
 
+        for student in Student_data_list:
+            if student.get('roll_no') == std_id:
+                Student_data_list.remove(student)
 
-
-
-
+                print("Student Deleted Successfully!")
+                found = True
+                break
+        if found == False:
+            print("Student Not Found!")
+        
